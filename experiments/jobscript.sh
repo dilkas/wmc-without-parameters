f6b0e805f320a39d3616d2dd29ed3d9bcadb6f1f
Paulius Dilkas <paulius.dilkas@gmail.com>
2020-07-18 20:22:48 +0100

    Proof that the encoding is correct

--- experiments/jobscript.sh
+++ experiments/jobscript.sh
@@ -6,8 +6,9 @@
 #$ -l h_vmem=8G
 
 . /etc/profile.d/modules.sh
-module load python/3.4.3
 module load java/jdk/1.8.0
+module load roslin/python/3.8.1
+module load phys/EXPERIMENTAL/compilers/gcc/10.1.0
 
 lscpu > cpuinfo.txt
 make -j 16
