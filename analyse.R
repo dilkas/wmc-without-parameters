435ea8971679109a40f2ffe59086200c0fd5aa06
Paulius Dilkas <paulius.dilkas@gmail.com>
2020-07-28 13:02:07 +0100

    Preliminary experimental results

--- analyse.R
+++ analyse.R
@@ -3,11 +3,13 @@ require(scales)
 require(dplyr)
 require(maditr)
 require(purrr)
+require(tikzDevice)
 
 TIMEOUT <- 1000
 df0 <- read.csv("results.csv", header = TRUE, sep = ",")
 df0$time[df0$time > TIMEOUT] <- TIMEOUT
 df0$time[is.na(df0$time)] <- TIMEOUT
+df0$memory[is.na(df0$memory)] <- max(df0$memory)
 df <- dcast(data = df0, formula = instance + dataset ~ encoding, fun.aggregate = sum, value.var = c("answer", "time", "memory"))
 df$major.dataset <- df$dataset
 df$major.dataset[df$dataset == "DQMR-50"] <- "DQMR"
@@ -17,47 +19,77 @@ df$major.dataset[df$dataset == "DQMR-100"] <- "DQMR"
 df$major.dataset[df$dataset == "Grid-50"] <- "Grid"
 df$major.dataset[df$dataset == "Grid-75"] <- "Grid"
 df$major.dataset[df$dataset == "Grid-90"] <- "Grid"
+df$time_min <- as.numeric(apply(df, 1, function (row) min(row["time_cd05"], row["time_cd06"],
+                                                          row["time_d02"], row["time_db20"], row["time_sbk05"])))
+
+# ============ Numerical investigations ================
 
 # Where answers don't match
 interesting <- df[abs(df$answer_db20 - df$answer_sbk05) > 0.01,]
 
-# Proportion of instances where my encoding is the best
-#time$min <- apply(time, 1, min)
-#sum(time$db20 == time$min)/nrow(time)
+# Unique
+sum(is.na(df$answer_db20) & !is.na(df$answer_cd05) & is.na(df$answer_cd06) &
+      is.na(df$answer_d02) & is.na(df$answer_sbk05)) # cd05
+sum(is.na(df$answer_db20) & is.na(df$answer_cd05) & !is.na(df$answer_cd06) &
+      is.na(df$answer_d02) & is.na(df$answer_sbk05)) # cd06
+sum(is.na(df$answer_db20) & is.na(df$answer_cd05) & is.na(df$answer_cd06) &
+      !is.na(df$answer_d02) & is.na(df$answer_sbk05)) # d02
+sum(!is.na(df$answer_db20) & is.na(df$answer_cd05) & is.na(df$answer_cd06) &
+      is.na(df$answer_d02) & is.na(df$answer_sbk05)) # db20
+sum(is.na(df$answer_db20) & is.na(df$answer_cd05) & is.na(df$answer_cd06) &
+      is.na(df$answer_d02) & !is.na(df$answer_sbk05)) # sbk05
 
-# Proportion unsolved
-sum(is.na(df$answer_cd05))/nrow(df)
-sum(is.na(df$answer_cd06))/nrow(df)
-sum(is.na(df$answer_d02))/nrow(df)
-sum(is.na(df$answer_db20))/nrow(df)
-sum(is.na(df$answer_sbk05))/nrow(df)
+# Fastest
+sum(!is.na(df$answer_cd05) & abs(df$time_cd05 - df$time_min) < 1e-5)
+sum(!is.na(df$answer_cd06) & abs(df$time_cd06 - df$time_min) < 1e-5)
+sum(!is.na(df$answer_d02) & abs(df$time_d02 - df$time_min) < 1e-5)
+sum(!is.na(df$answer_db20) & abs(df$time_db20 - df$time_min) < 1e-5)
+sum(!is.na(df$answer_sbk05) & abs(df$time_sbk05 - df$time_min) < 1e-5)
+
+# Solved
+sum(!is.na(df$answer_cd05))
+sum(!is.na(df$answer_cd06))
+sum(!is.na(df$answer_d02))
+sum(!is.na(df$answer_db20))
+sum(!is.na(df$answer_sbk05))
+
+# ================ Plots ==========================
 
 # Scatter plot: by dataset
 # Consider:
 # 1. Shapes as well as colors (set manually).
 # 2. Adjust the alpha value.
-
-# D02
+# TODO: consider gray log ticks
+# TODO: merge this into a single plot, with a legend either to the side or inside each plot
 min.time <- min(df0$time) + 0.001
+
+# d02
+tikz(file = "paper/scatter_d02.tex", width = 4.8, height = 3)
 ggplot(df[df$time_d02 > 0,], aes(x = time_db20, y = time_d02, col = major.dataset)) +
   geom_point(alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0) +
-  scale_x_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT)) +
-  scale_y_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT)) +
+  scale_x_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT), breaks = c(0.1, 10, 1000), labels = c("0.1", "10", "1000")) +
+  scale_y_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT), breaks = c(0.1, 10, 1000), labels = c("0.1", "10", "1000")) +
   xlab("db20 time (s)") +
   ylab("d02 time (s)") +
   scale_color_brewer(palette = "Dark2", name = "Data set") +
-  coord_fixed()
+  coord_fixed() +
+  annotation_logticks()
+dev.off()
 
-ggplot(df, aes(x = time_db20, y = time_sbk05, col = major.dataset)) +
+# sbk05
+tikz(file = "paper/scatter_sbk05.tex", width = 4.8, height = 3)
+ggplot(df[df$time_sbk05 > 0,], aes(x = time_db20, y = time_sbk05, col = major.dataset)) +
   geom_point(alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0) +
-  scale_x_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT)) +
-  scale_y_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT)) +
+  scale_x_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT), breaks = c(0.1, 10, 1000), labels = c("0.1", "10", "1000")) +
+  scale_y_continuous(trans = log10_trans(), limits = c(min.time, TIMEOUT), breaks = c(0.1, 10, 1000), labels = c("0.1", "10", "1000")) +
   xlab("db20 time (s)") +
   ylab("sbk05 time (s)") +
   scale_color_brewer(palette = "Dark2", name = "Data set") +
-  coord_fixed()
+  coord_fixed() +
+  annotation_logticks()
+dev.off()
 
 # Scatter plot: for a specific dataset
 ggplot(df[df$dataset == "2004-PGM"], aes(x = time_db20, y = time_sbk05)) +
@@ -67,7 +99,6 @@ ggplot(df[df$dataset == "2004-PGM"], aes(x = time_db20, y = time_sbk05)) +
   ylim(0, TIMEOUT)
 
 # Cumulative plot
-
 times <- unique(df0$time)
 cumulative <- rbind.data.frame(cbind(times, "cd05", unlist(times %>% map(function(x) sum(df0$time[df0$encoding == "cd05"] <= x)))),
       cbind(times, "cd06", unlist(times %>% map(function(x) sum(df0$time[df0$encoding == "cd06"] <= x)))),
@@ -80,13 +111,16 @@ cumulative$time <- as.numeric(cumulative$time)
 cumulative$count <- as.numeric(cumulative$count)
 cumulative <- cumulative[cumulative$time < TIMEOUT, ]
 
+tikz(file = "paper/cumulative.tex", width = 4.8, height = 3)
 ggplot(cumulative, aes(x = time, y = count, color = encoding)) +
   geom_line() +
-  scale_x_continuous(trans = log10_trans()) +
+  scale_x_continuous(trans = log10_trans(), breaks = c(0.1, 10, 1000), labels = c("0.1", "10", "1000")) +
   xlab("Time (s)") +
   ylab("Instances solved") +
   labs(color = "Encoding") +
-  scale_colour_brewer(palette = "Dark2")
+  scale_colour_brewer(palette = "Dark2") +
+  annotation_logticks(sides = "b")
+dev.off()
 
 # Scatter plot for memory usage (not interesting)
 ggplot(df, aes(x = memory_db20, y = memory_cd06, col = major.dataset)) +
