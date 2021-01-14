421033cb56b33a0af8c9c1b1983b8750a572d514
Paulius Dilkas <paulius.dilkas@gmail.com>
2021-01-14 08:09:59 +0000

    CNF4DPMC v1

--- tools/cnf4dpmc/cnf4dpmc.cpp
+++ tools/cnf4dpmc/cnf4dpmc.cpp
@@ -43,37 +43,36 @@ int main(int argc, char *argv[]) {
     if (token != "I") continue;
     std::getline(iss, token, '$');
     int indicator = std::stoi(token);
-    for (int parameter = previous_indicator + 1; parameter < indicator; parameter++) {
+    for (int parameter = previous_indicator + 1; parameter < indicator; parameter++)
       decrements[parameter] = ++decrement;
-      std::cout << "parameter variable: " << parameter << ", its decrement: " << decrement << std::endl;
-    }
     previous_indicator = indicator;
   }
-  for (int parameter = previous_indicator + 1; parameter <= num_vars; parameter++) {
+  for (int parameter = previous_indicator + 1; parameter <= num_vars; parameter++)
     decrements[parameter] = ++decrement;
-    std::cout << "parameter variable: " << parameter << ", its decrement: " << decrement << std::endl;
-  }
 
-  // Translate the CNF
+  // Read the CNF file, translating variable names, removing some clauses and
+  // turning some other clauses into weight lines.
   std::string cnf_filename = bn_filename + ".cnf";
   std::ifstream cnf_file(cnf_filename);
+  std::map<int,std::string> weights;
+  std::vector<std::vector<int>> new_literals;
+  std::vector<int> new_parameters;
+  int num_clauses = 0;
   while (std::getline(cnf_file, line)) {
     std::istringstream iss(line);
     std::string token;
     iss >> token;
     if (token == "c") {
-      // TODO: Read the weights of parameter variables into a map. Otherwise ignore the line.
       iss >> token;
-      std::ostringstream oss;
       for (int i = 1; i <= 2 * num_vars; i++) {
         iss >> token;
-        auto it = decrements.find((i+1)/2);
-        if (it == decrements.end()) {
-          oss << " " << token;
-        }
+        int variable = (i+1)/2;
+        if (i % 2 == 1 && decrements.find(variable) != decrements.end())
+          weights[variable] = token;
       }
     } else if (token[0] != 'p') {
       std::vector<int> literals;
+      int parameter_variable = 0;
       LineType line_type = Clause;
       while (token != "0") {
         int literal = std::stoi(token);
@@ -83,21 +82,32 @@ int main(int argc, char *argv[]) {
         }
         if (literal > 0 && decrements.find(literal) != decrements.end()) {
           line_type = Weight;
+          parameter_variable = literal;
         } else {
           literals.push_back(rename_literal(literal, decrements));
         }
         iss >> token;
       }
-      if (line_type == Clause) {
-        for (int literal : literals)
-          std::cout << literal << " ";
-        std::cout << "0" << std::endl;
-      } else if (line_type == Weight) {
-        std::cout << "w";
-        for (int literal : literals)
-          std::cout << " " << -literal;
-        std::cout << std::endl;
+      if (line_type != Skip) {
+        new_literals.push_back(literals);
+        new_parameters.push_back(parameter_variable);
+        if (line_type == Clause) num_clauses++;
       }
+    }
+  }
+
+  // Compile and output the new encoding
+  std:: cout << "p cnf " << num_vars - decrements.size() << " " << num_clauses << std::endl;
+  for (int i = 0; i < new_literals.size(); i++) {
+    if (new_parameters[i] == 0) {
+      for (int literal : new_literals[i])
+        std::cout << literal << " ";
+      std::cout << "0" << std::endl;
+    } else {
+      std::cout << "w";
+      for (int literal : new_literals[i])
+        std::cout << " " << -literal;
+      std::cout << " " << weights[new_parameters[i]] << std::endl;
     }
   }
 }
