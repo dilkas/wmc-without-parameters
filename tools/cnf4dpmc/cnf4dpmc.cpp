1e81604e1e2285c100c8f90549f8f5e8908d8fff
Paulius Dilkas <paulius.dilkas@gmail.com>
2021-01-25 10:45:18 +0000

    Make some variables global

--- tools/cnf4dpmc/cnf4dpmc.cpp
+++ tools/cnf4dpmc/cnf4dpmc.cpp
@@ -5,9 +5,17 @@
 
 enum LineType {Clause, Weight, Skip};
 
-int RenameLiteral(int literal, const std::map<int, int> &decrements) {
+int num_clauses = 0;
+int num_vars = 0;
+std::map<int, int> decrements;
+std::vector<std::vector<int>> new_literals;
+std::vector<int> new_parameters;
+std::vector<std::string> new_weights;
+std::map<int, std::string> weights;
+
+int RenameLiteral(int literal) {
   if (literal < 0) {
-    return -RenameLiteral(-literal, decrements);
+    return -RenameLiteral(-literal);
   }
   if (decrements.empty() || literal < decrements.begin()->first) {
     return literal;
@@ -18,17 +26,17 @@ int RenameLiteral(int literal, const std::map<int, int> &decrements) {
   return new_literal;
 }
 
-void AddToDecrements(int literal, std::map<int, int> *decrements) {
-  auto it = decrements->upper_bound(literal);
-  int decrement = (it != decrements->begin()) ? std::prev(it)->second + 1 : 1;
-  for (; it != decrements->end(); it++) {
-    (*decrements)[it->first] = it->second + 1;
+void AddToDecrements(int literal) {
+  auto it = decrements.upper_bound(literal);
+  int decrement = (it != decrements.begin()) ? std::prev(it)->second + 1 : 1;
+  for (; it != decrements.end(); it++) {
+    decrements[it->first] = it->second + 1;
   }
-  (*decrements)[literal] = decrement;
+  decrements[literal] = decrement;
 }
 
-void Rename(int previous, int next, std::vector<std::vector<int>> *clauses) {
-  for (auto & clause : *clauses) {
+void Rename(int previous, int next) {
+  for (auto & clause : new_literals) {
     for (auto & literal : clause) {
       if (literal == previous) {
         literal = next;
@@ -40,12 +48,11 @@ void Rename(int previous, int next, std::vector<std::vector<int>> *clauses) {
 }
 
 // Read the LMAP file, creating a map of decrements
-int CreateDecrements(std::string bn_filename, std::map<int, int> *decrements) {
+void CreateDecrements(std::string bn_filename) {
   std::string lmap_filename = bn_filename + ".lmap";
   std::ifstream lmap_file(lmap_filename);
   std::string line;
   int previous_indicator = 0;
-  int num_vars = 0;
   int decrement = 0;
   while (std::getline(lmap_file, line)) {
     std::istringstream iss(line);
@@ -70,26 +77,20 @@ int CreateDecrements(std::string bn_filename, std::map<int, int> *decrements) {
     }
     for (int parameter = previous_indicator + 1; parameter < indicator;
          parameter++) {
-      (*decrements)[parameter] = ++decrement;
+      decrements[parameter] = ++decrement;
     }
     previous_indicator = indicator;
   }
   for (int parameter = previous_indicator + 1; parameter <= num_vars;
        parameter++) {
-    (*decrements)[parameter] = ++decrement;
+    decrements[parameter] = ++decrement;
   }
-  return num_vars;
 }
 
 // Read the CNF file, translating variable names, removing some clauses and
 // turning some other clauses into weight lines.
-int ParseCnf(std::string cnf_filename, int num_vars,
-             const std::map<int, int> &decrements,
-             std::vector<std::vector<int>> *new_literals,
-             std::vector<int> *new_parameters,
-             std::map<int, std::string> *weights) {
+void ParseCnf(std::string cnf_filename) {
   std::ifstream cnf_file(cnf_filename);
-  int num_clauses = 0;
   std::string line;
   while (std::getline(cnf_file, line)) {
     std::istringstream iss(line);
@@ -101,7 +102,7 @@ int ParseCnf(std::string cnf_filename, int num_vars,
         iss >> token;
         int variable = (i+1)/2;
         if (i % 2 == 1 && decrements.find(variable) != decrements.end()) {
-          (*weights)[variable] = token;
+          weights[variable] = token;
         }
       }
     } else if (token[0] != 'p') {
@@ -123,51 +124,45 @@ int ParseCnf(std::string cnf_filename, int num_vars,
         iss >> token;
       }
       if (line_type != Skip) {
-        new_literals->push_back(literals);
-        new_parameters->push_back(parameter_variable);
+        new_literals.push_back(literals);
+        new_parameters.push_back(parameter_variable);
         if (!literals.empty()) {
           num_clauses++;
         }
       }
     }
   }
-  return num_clauses;
 }
 
 // Let's not use two 'bits' to represent two possible values
-std::vector<std::string> MergeVariables(std::map<int, int> *decrements,
-                                        std::vector<std::vector<int>> *new_literals,
-                                        std::vector<int> *new_parameters,
-                                        int *num_clauses,
-                                        std::map<int, std::string> *weights) {
-  std::vector<std::string> new_weights;
-  assert(new_literals->size() == new_parameters->size());
-  for (size_t i = 0; i < new_literals->size() - 1; i++) {
-    if ((*new_parameters)[i] == 0 && (*new_parameters)[i+1] == 0 &&
-        (*new_literals)[i].size() == 2 && (*new_literals)[i+1].size() == 2 &&
-        (((*new_literals)[i+1][0] == -(*new_literals)[i][0] &&
-          (*new_literals)[i+1][1] == -(*new_literals)[i][1]) ||
-         ((*new_literals)[i+1][0] == -(*new_literals)[i][1] &&
-          (*new_literals)[i+1][1] == -(*new_literals)[i][0]))) {
+void MergeVariables() {
+  assert(new_literals.size() == new_parameters.size());
+  for (size_t i = 0; i < new_literals.size() - 1; i++) {
+    if (new_parameters[i] == 0 && new_parameters[i+1] == 0 &&
+        new_literals[i].size() == 2 && new_literals[i+1].size() == 2 &&
+        ((new_literals[i+1][0] == -new_literals[i][0] &&
+          new_literals[i+1][1] == -new_literals[i][1]) ||
+         (new_literals[i+1][0] == -new_literals[i][1] &&
+          new_literals[i+1][1] == -new_literals[i][0]))) {
       int num_to_remove = 2;
-      int a = std::min(std::abs((*new_literals)[i][0]),
-                       std::abs((*new_literals)[i][1]));
-      int b = std::max(std::abs((*new_literals)[i][0]),
-                       std::abs((*new_literals)[i][1]));
-      if (i + 3 < new_parameters->size() &&
-          (*new_parameters)[i+2] != 0 && (*new_parameters)[i+3] != 0 &&
-          (*new_literals)[i+2].size() == 1 && (*new_literals)[i+3].size() == 1) {
+      int a = std::min(std::abs(new_literals[i][0]),
+                       std::abs(new_literals[i][1]));
+      int b = std::max(std::abs(new_literals[i][0]),
+                       std::abs(new_literals[i][1]));
+      if (i + 3 < new_parameters.size() &&
+          new_parameters[i+2] != 0 && new_parameters[i+3] != 0 &&
+          new_literals[i+2].size() == 1 && new_literals[i+3].size() == 1) {
         num_to_remove = 4;
 
         // Determine the weights of a and b
         std::string a_weight;
         std::string b_weight;
-        if ((*new_literals)[i+2][0] == -a && (*new_literals)[i+3][0] == -b) {
-          a_weight = (*weights)[(*new_parameters)[i+2]];
-          b_weight = (*weights)[(*new_parameters)[i+3]];
-        } else if ((*new_literals)[i+2][0] == -b && (*new_literals)[i+3][0] == -a) {
-          a_weight = (*weights)[(*new_parameters)[i+3]];
-          b_weight = (*weights)[(*new_parameters)[i+2]];
+        if (new_literals[i+2][0] == -a && new_literals[i+3][0] == -b) {
+          a_weight = weights[new_parameters[i+2]];
+          b_weight = weights[new_parameters[i+3]];
+        } else if (new_literals[i+2][0] == -b && new_literals[i+3][0] == -a) {
+          a_weight = weights[new_parameters[i+3]];
+          b_weight = weights[new_parameters[i+2]];
         } else {
           continue;
         }
@@ -176,35 +171,28 @@ std::vector<std::string> MergeVariables(std::map<int, int> *decrements,
         // NOTE: we assume that all variables that are due to be removed that
         // are smaller than 'a' have already been added to 'decrements'
         std::ostringstream oss;
-        oss << "w " << RenameLiteral(a, *decrements) << " " << a_weight << " "
+        oss << "w " << RenameLiteral(a) << " " << a_weight << " "
             << b_weight;
         new_weights.push_back(oss.str());
         num_clauses++;
       }
-      Rename(b, (((*new_literals)[i][0] < 0 && (*new_literals)[i][1] < 0) ||
-                 ((*new_literals)[i][0] > 0 && (*new_literals)[i][1] > 0)) ? -a : a,
-             new_literals);
-      AddToDecrements(b, decrements);
+      Rename(b, ((new_literals[i][0] < 0 && new_literals[i][1] < 0) ||
+                 (new_literals[i][0] > 0 && new_literals[i][1] > 0)) ? -a : a);
+      AddToDecrements(b);
 
       // Remove clauses
       num_clauses -= num_to_remove;
-      new_literals->erase(new_literals->begin() + i,
-                          new_literals->begin() + i + num_to_remove);
-      new_parameters->erase(new_parameters->begin() + i,
-                            new_parameters->begin() + i + num_to_remove);
+      new_literals.erase(new_literals.begin() + i,
+                         new_literals.begin() + i + num_to_remove);
+      new_parameters.erase(new_parameters.begin() + i,
+                           new_parameters.begin() + i + num_to_remove);
       i--;
     }
   }
-  return new_weights;
 }
 
 // Compile and output the new encoding
-void OutputEncoding(std::string cnf_filename, int num_clauses, int num_vars,
-                    const std::map<int, int> &decrements,
-                    const std::vector<std::vector<int>> &new_literals,
-                    const std::vector<int> &new_parameters,
-                    const std::vector<std::string> &new_weights,
-                    std::map<int, std::string> *weights) {
+void OutputEncoding(std::string cnf_filename) {
   std::ofstream output(cnf_filename);
   output << "p cnf " << num_vars - decrements.size() << " " << num_clauses
          << std::endl;
@@ -212,17 +200,17 @@ void OutputEncoding(std::string cnf_filename, int num_clauses, int num_vars,
   for (size_t i = 0; i < new_literals.size(); i++) {
     if (new_parameters[i] == 0) {
       for (int literal : new_literals[i]) {
-        output << RenameLiteral(literal, decrements) << " ";
+        output << RenameLiteral(literal) << " ";
       }
       output << "0" << std::endl;
     } else if (new_literals[i].empty()) {
-      premultiplication_constant *= std::stod((*weights)[new_parameters[i]]);
+      premultiplication_constant *= std::stod(weights[new_parameters[i]]);
     } else {
       output << "w";
       for (int literal : new_literals[i]) {
-        output << " " << RenameLiteral(-literal, decrements);
+        output << " " << RenameLiteral(-literal);
       }
-      output << " " << (*weights)[new_parameters[i]] << " 1" << std::endl;
+      output << " " << weights[new_parameters[i]] << " 1" << std::endl;
     }
   }
   for (const std::string &weight_line : new_weights) {
@@ -245,14 +233,8 @@ int main(int argc, char *argv[]) {
   std::string bn_filename = result["filename"].as<std::string>();
   std::string cnf_filename = bn_filename + ".cnf";
 
-  std::map<int, int> decrements;
-  int num_vars = CreateDecrements(bn_filename, &decrements);
-
-  std::map<int, std::string> weights;
-  std::vector<std::vector<int>> new_literals;
-  std::vector<int> new_parameters;
-  int num_clauses = ParseCnf(cnf_filename, num_vars, decrements, &new_literals,
-                             &new_parameters, &weights);
+  CreateDecrements(bn_filename);
+  ParseCnf(cnf_filename);
 
   // Weight lines with weight = 1 can be removed
   for (size_t i = 0; i < new_literals.size(); i++) {
@@ -265,8 +247,6 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  auto new_weights = MergeVariables(&decrements, &new_literals, &new_parameters,
-                                    &num_clauses, &weights);
-  OutputEncoding(cnf_filename, num_clauses, num_vars, decrements, new_literals,
-                 new_parameters, new_weights, &weights);
+  MergeVariables();
+  OutputEncoding(cnf_filename);
 }
